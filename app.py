fb1a7756ff5d0fc93391d68562b76edf6d2d526b
caogreg <cao.gregory@gmail.com>
2020-08-07 22:34:09 -0400

    Adding user view of applied jobs and user profile modification

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sshtunnel import SSHTunnelForwarder
 import pymysql
 import os
 
+from db_connection import get_job_applications
 
 app = Flask(__name__)
 bootstrap = Bootstrap()
@@ -58,9 +59,51 @@ def postings():
 
 @app.route('/appliedJobs')
 def applied_jobs():
-    return render_template('appliedJobs.html')
+    email = None
+    open('templates/applied-jobs-results.html', 'w').close()
 
-@app.route('/settings')
+    if request.method == 'POST':
+        email = "aoyama@weeb.com"
+
+        file = open('templates/applied-jobs-results.html')
+        html = """
+        {% extends 'base.html' %}{% block body %}
+        <div class="container"><h1>Job Search Page</h1><br><form id="job-search-engine" action="" method="post"><input type="text" placeholder="Job Title" name="title_search"><input type="text" placeholder="Category" name="category_search"><input class="btn btn-success" type="submit" value="Search"></form><br><br></div>
+        <div class="container">
+            <table class="table" id="posting-table">
+              <thead>
+                <tr>
+                  <th scope="col">Posting ID</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Job Title</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Posting Date</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Category</th>
+                </tr>
+              </thead>
+              <tbody>
+        """
+        results = get_job_applications(email)
+
+        for rows in results:
+            html += "<tr>"
+            for col in rows:
+                html += "<td>" + str(col) + "</td>\n"
+            html += "</tr>"
+        html += """
+            </tbody>
+        </table>
+        </div>
+        {% endblock %}
+        """
+        file.write(html)
+        file.close()
+        return render_template('applied-jobs-results.html')
+    else:
+        return render_template('appliedJobs.html')
+
+@app.route('/settings', methods=['GET', 'POST'])
 def settings():
     return render_template('settings.html')
 
